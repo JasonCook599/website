5b1b917e91006de8273543c75a691edf8c4f408f
JasonCook599 <JasonCook599@users.noreply.github.com>
2021-10-29 17:01:14 -0400

    Headers / JS & CSS
    Fixed headers
    Remove inline JS & CSS
    Minify JS & CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,25 @@
 const CleanCSS = require("clean-css");
-const { minify } = require("terser");
+const { minify }  = require("terser");
+const htmlmin = require("html-minifier");
 
-
-module.exports = function(config) {
-  config.addFilter("cssmin", function(code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
-
-  config.addPassthroughCopy("assets");
-
-  config.addNunjucksAsyncFilter("jsmin", async function (
-    code,
-    callback
-  ) {
-    try {
-      const minified = await minify(code);
-      callback(null, minified.code);
-    } catch (err) {
-      console.error("Terser error: ", err);
-      // Fail gracefully.
-      callback(null, code);
-    }
-  });
-}
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addTransform("minify-output", async function(content, outputPath) {
+        // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+        if (outputPath && outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        } else if (outputPath && outputPath.endsWith(".css")) {
+            return new CleanCSS().minify(content).styles
+        } else if (outputPath && outputPath.endsWith(".js")) {
+            var result = await minify(content);
+            return result.code
+        } else {
+            return content;
+        }
+    });
+};
