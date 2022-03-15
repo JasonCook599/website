d0ce5e3d33afeba4af1673d9eb15dc73d37fd638
JasonCook599 <JasonCook599@users.noreply.github.com>
2022-03-15 14:22:01 -0400

    Update time page
    Use Day.js for time/date formatting
    Use fitty for auto resizing
    Update formatting. Use Roboto Mono font.

--- _includes/script.js
+++ _includes/script.js
@@ -1,18 +1,14 @@
-function sleep(milliseconds) {
-  const date = Date.now()
-  let currentDate = null
-  do {
-    currentDate = Date.now()
-  } while (currentDate - date < milliseconds)
-}
 async function getRemoteTime() {
   return new Date((await (await fetch("/api/date")).json()).date)
 }
+
 async function getTimeOffset(interval, oldoffset) {
   if (remoteTimeLastCheck == null || remoteTimeLastCheck < new Date(Date.now() - interval)) {
     remoteTimeLastCheck = Date.now()
     try {
-      return ((await getRemoteTime()) - new Date())
+      offset = (await getRemoteTime()) - new Date()
+      console.log("Your clock is off by " + offset + "ms")
+      return offset
     } catch (error) {
       console.log(error)
       return 0
@@ -21,6 +17,7 @@ async function getTimeOffset(interval, oldoffset) {
     return oldoffset
   }
 }
+
 async function getPreferences() {
   try {
     return await (await fetch("/assets/prefs.json")).json()
@@ -29,60 +26,34 @@ async function getPreferences() {
     return
   }
 }
+
 async function updateTime() {
-  offset = await getTimeOffset(60 * 60 * 1000, offset) // Get time offset. Interval of one hours. 60 seconds * 60 minutes * 1000 milliseconds per second
-  prefs = await getPreferences() // Get preferences
+  // Get time offset once per hour. 3600000 = 60 seconds * 60 minutes * 1000 milliseconds
+  offset = await getTimeOffset(3600000, offset)
+
+  // Get preferences
+  prefs = await getPreferences()
+
   //Set date based on offset
   var date = new Date(Date.now() + offset)
-  var hour = date.getHours() // 0 - 23
-  var minute = date.getMinutes() // 0 - 59
-  var second = date.getSeconds() // 0 - 59
-  //convert to 12h
-  if (prefs.twelvehour === true) {
-    hour = hour % 12 // mod 12
-    hour = hour ? hour : 12 //midnight
-    session = hour >= 12 ? prefs.format.session.PM : prefs.format.session.AM //AM or PM
-  }
-  //Add leading zeros.
-  if (prefs.leadingzeros.hour) {
-    var hour = hour.toString().padStart(2, 0)
-  }
-  if (prefs.leadingzeros.minute) {
-    var minute = minute.toString().padStart(2, 0)
-  }
-  if (prefs.leadingzeros.second) {
-    var second = second.toString().padStart(2, 0)
-  }
-  // Format time based on prefs
-  time = null
-  if (prefs.show.hour) {
-    time += hour
-  }
-  if (prefs.show.hour & prefs.show.minute) {
-    time += prefs.separator
-  }
-  if (prefs.show.minute) {
-    time += minute
-  }
-  if (prefs.show.minute & prefs.show.second) {
-    time += prefs.separator
-  }
-  if (prefs.show.second) {
-    time += second
-  }
-  if (prefs.show.session && prefs.twelvehour) {
-    time += " " + session
-  }
-  document.getElementById("time").textContent = time //Show time
-  // TODO Add formatting options for date.
-  document.getElementById("date").textContent = date.toDateString() //Show date
-  setTimeout(updateTime, 500) // Wait, then update time again
+
+  // https://day.js.org/docs/en/display/format#list-of-all-available-formats
+  timeTitle = dayjs(date).format(prefs.titleFormat)
+  timeSubtitle = dayjs(date).format(prefs.subtitleFormat)
+
+  // Show time and date
+  document.getElementById("title").textContent = timeTitle
+  document.getElementById("subtitle").textContent = timeSubtitle
+  fitty("#title")
+
+  // Wait, then update time again
+  setTimeout(updateTime, 250)
 }
 window.onload = load
 
 function load() {
   // Only run clock code on clock page.
-  if (document.getElementById("time")) {
+  if (document.getElementById("clock")) {
     remoteTimeLastCheck = null
     offset = null
     updateTime()
