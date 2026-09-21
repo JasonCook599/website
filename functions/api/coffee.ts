b78c4274018d04d17b7308a803b7d9c44e0da863
Jason Cook <JasonCook599@users.noreply.github.com>
2026-09-21 11:56:31 -0400

    Prettier & npm update

--- functions/api/coffee.ts
+++ functions/api/coffee.ts
@@ -1,4 +1,4 @@
-import {ApiHeaders} from "./headers";
+import { ApiHeaders } from "./headers";
 
 export async function onRequest(context) {
   // Contents of context object
@@ -8,7 +8,7 @@ export async function onRequest(context) {
     params, // if filename includes [id] or [[path]]
     waitUntil, // same as ctx.waitUntil in existing Worker API
     next, // used for middleware or to fetch assets
-    data // arbitrary space for passing data between middlewares
+    data, // arbitrary space for passing data between middlewares
   } = context;
   // Function to parse query strings
   function getParameterByName(name) {
@@ -17,10 +17,8 @@ export async function onRequest(context) {
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
       results = regex.exec(request.url);
 
-    if (!results) 
-      return null;
-    else if (!results[2]) 
-      return "";
+    if (!results) return null;
+    else if (!results[2]) return "";
     else if (results[2]) {
       results[2] = results[2].replace(/\//g, "");
     }
@@ -30,29 +28,32 @@ export async function onRequest(context) {
   const location = getParameterByName("location");
 
   if (request.method === "OPTIONS") {
-    return new Response("OK", {headers: ApiHeaders(request)});
+    return new Response("OK", { headers: ApiHeaders(request) });
   } else if (request.method === "GET") {
     // concat CORS and application
     const responseHeaders = {
       ...ApiHeaders(request),
-      "content-type": "application/json;charset=UTF-8"
+      "content-type": "application/json;charset=UTF-8",
     };
     const coffee = JSON.parse(await context.env.coffee.get(location));
-    const responseJson = JSON.stringify({location: location, date: coffee.date});
-    return new Response(responseJson, {headers: responseHeaders});
+    const responseJson = JSON.stringify({
+      location: location,
+      date: coffee.date,
+    });
+    return new Response(responseJson, { headers: responseHeaders });
   } else if (request.method === "POST") {
     // TODO add authentication
     // create json response
     const dateJson = JSON.stringify({
-      date: new Date(Date.now())
+      date: new Date(Date.now()),
     });
 
     await context.env.coffee.put(location, dateJson);
-    return new Response("OK", {headers: ApiHeaders(request)});
+    return new Response("OK", { headers: ApiHeaders(request) });
   } else {
     return new Response("Bad Request", {
       status: 400,
-      headers: ApiHeaders(request)
+      headers: ApiHeaders(request),
     });
   }
 }
